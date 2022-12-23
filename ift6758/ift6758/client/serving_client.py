6715d5bdf98b569eb73e373b24b0b807340a30d2
Vijeet24 <86227117+Vijeet24@users.noreply.github.com>
2022-12-23 23:55:10 +0700

    Update serving_client.py

--- ift6758/ift6758/client/serving_client.py
+++ ift6758/ift6758/client/serving_client.py
@@ -2,20 +2,23 @@ import json
 import requests
 import pandas as pd
 import logging
+from ift6758.data.tidyData_adv import tidyData_adv
+from ift6758.data.functions import pre_process
+
+# import ift6758.data.tidyData_adv as tidyData_adv
 
 
 logger = logging.getLogger(__name__)
 
 
 class ServingClient:
-    def __init__(self, ip: str = "0.0.0.0", port: int = 5000, features=None):
+    def __init__(self, ip: str = "0.0.0.0", port: int = 8080, features=None):
         self.base_url = f"http://{ip}:{port}"
-        logger.info(f"Initializing client; base URL: {self.base_url}")
+        logger.info(f"Initializing serving client; base URL: {self.base_url}")
 
         if features is None:
             features = ["distance"]
         self.features = features
-
         # any other potential initialization
 
     def predict(self, X: pd.DataFrame) -> pd.DataFrame:
@@ -27,22 +30,35 @@ class ServingClient:
         Args:
             X (Dataframe): Input dataframe to submit to the prediction service.
         """
+        # X = tidyData_adv(X)
+        # X = pre_process(X)
+        # X = X.drop(['isGoal'], axis=1)
+        # X = X[self.features]
 
-        raise NotImplementedError("TODO: implement this function")
+        r = requests.post(
+        	f"{self.base_url}/predict", 
+        	json = X.to_dict()
+        )
+        # print(r)
+        # print(r.json())
+        return pd.DataFrame(r.json())
 
     def logs(self) -> dict:
         """Get server logs"""
+        #request server api
+        r = requests.get(f"{self.base_url}/logs")
+        #print(r)
+        logs = r.json()
+        print(logs)
 
-        raise NotImplementedError("TODO: implement this function")
+        return logs
 
     def download_registry_model(self, workspace: str, model: str, version: str) -> dict:
         """
         Triggers a "model swap" in the service; the workspace, model, and model version are
         specified and the service looks for this model in the model registry and tries to
         download it. 
-
         See more here:
-
             https://www.comet.ml/docs/python-sdk/API/#apidownload_registry_model
         
         Args:
@@ -51,4 +67,24 @@ class ServingClient:
             version (str): The model version to download
         """
 
-        raise NotImplementedError("TODO: implement this function")
+        req = {
+            'workspace': workspace,
+            'model': model,
+            'version': version,
+        }
+
+        r = requests.post(
+        	f"{self.base_url}/download_registry_model", 
+        	json=json.loads(json.dumps(req, indent = 4))
+        )
+        
+        logs = r.json()
+        print(logs)
+
+        return logs
+
+# abc = ServingClient()
+# X = pd.DataFrame({'distance': [1,2,3], 'isGoal': [2,3,4]})
+# #X = pd.DataFrame[{'Distance': [1,2,3,4,5,6,7,8,9,10], 'isGoal': [0,0,0,1,0,0,0,0,0,0]}]
+# print(X.shape)
+# abc.predict(X)
