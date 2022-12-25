2eaea96c8be19703879f5ffb546b754fc0621082
Vijeet24 <86227117+Vijeet24@users.noreply.github.com>
2022-12-26 03:08:04 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,45 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from serving import ServingClient
+from serving import GameClient
+import json
 
-"""
-General template for your streamlit app. 
-Feel free to experiment with layout and adding functionality!
-Just make sure that the required functionality is included as well
-"""
-
-st.title("INSERT APP TITLE HERE")
+st.title("Hockey Visualization App")
+sc = ServingClient.ServingClient(ip="127.0.0.1", port=5000)
+gc = GameClient.GameClient()
 
 with st.sidebar:
-    # TODO: Add input for the sidebar
+    workspace = st.text_input('Workspace', 'ift-6758-2')
+    model = st.text_input('Model', 'xgb-model-5-3-pickle')
+    version = st.text_input('Version', '1.0.0')
+    if st.button('Get Model'):
+
+        sc.download_registry_model(
+            workspace=workspace,
+            model=model,
+            version=version
+        )
+        st.write('Downloaded')
     pass
 
-with st.container():
-    # TODO: Add Game ID input
-    pass
+
+def ping_game_id(game_id):
+    with st.container():
+        # TODO: Add Game info and predictions
+        filepath = gc.get_game(game_id=game_id)
+        model_df, last_event_df = gc.ping_game(filepath)
+
+        pass
+    with st.container():
+        # TODO: Add data used for predictions
+        st.subheader("Data used for predictions (and predictions)")
+        st.table(model_df)
+        pass
+
 
 with st.container():
-    # TODO: Add Game info and predictions
-    pass
-
-with st.container():
-    # TODO: Add data used for predictions
+    game_id = st.text_input('Game ID', '2022020329')
+    if st.button('Ping game'):
+        ping_game_id(game_id)
     pass
